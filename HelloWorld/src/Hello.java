e1142ea60ca367c47197c6ca8cf34a08d3cae1ba
GSISP <pushpalatha.gadiraju@gmail.com>
2015-04-28 06:58:57 +0530

    First Sample Code

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,28 @@
+
+public class Hello {
+
+	//block
+	static{
+		System.out.print("static block executed...");
+	}
+	
+	//method
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main method executed...");
+		
+		float f=12.345f;
+		int i=(int) f;
+		System.out.println(i);//0.345
+		
+		byte b=(byte) 200;//-128..-127..-126.....72nd to 127 ----> 72
+		System.out.println(b);	
+		
+		
+	}
+	
+	{
+		System.out.print("non static block executed...");
+	}
+
+}
